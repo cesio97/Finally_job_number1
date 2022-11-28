1320b8c1d1bece3bf992843e69ac78392eb36bdb
cesio97 <114251017+cesio97@users.noreply.github.com>
2022-11-28 18:41:17 +0300

    Add files via upload

---
+++ Task_control/task/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите длинну массива:");
+int N =int.Parse(Console.ReadLine());
+string[] array = new string[N];
+
+newArray(array);
+printArray(array);
+printNewArray(array);
+
+void newArray(string[]array)
+{
+  Console.WriteLine("Введите массив строк:");
+    for (int i=0;i<array.Length;i++)
+    {
+      Console.Write($"{array[i]}");
+      array[i] = Console.ReadLine();
+    }
+}
+
+void printArray(string[]array)
+{
+  Console.WriteLine("Первоначальный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+          if (i < (array.Length -1)) Console.Write($", ");
+    }
+  Console.WriteLine(" ");
+}
+
+void printNewArray(string[]array)
+{
+  Console.WriteLine("Новый массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) Console.Write($"{array[i]}");
+        if (i < (array.Length -2)) Console.Write($", ");
+    }
+  Console.WriteLine(" ");
+}
